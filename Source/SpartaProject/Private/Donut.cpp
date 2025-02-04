95a591f833a5b29ac815dd3a154d6efbd396275e
aiden_shin <shinichi9784@gmail.com>
2025-02-05 06:53:00 +0900

    커밋

---
+++ Source/SpartaProject/Private/Donut.cpp
@@ -0,0 +1,68 @@
+#include "Donut.h"
+
+ADonut::ADonut()
+{
+    SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+    SetRootComponent(SceneRoot);
+
+    StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+    StaticMeshComp->SetupAttachment(SceneRoot);
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/Resources/Shapes/Shape_Torus.Shape_Torus"));
+    if (MeshAsset.Succeeded())
+    {
+        StaticMeshComp->SetStaticMesh(MeshAsset.Object);
+    }
+
+    static ConstructorHelpers::FObjectFinder<UMaterial> MaterialAsset(TEXT("/Game/Resources/Materials/M_Tech_Hex_Tile.M_Tech_Hex_Tile"));
+    if (MaterialAsset.Succeeded())
+    {
+        StaticMeshComp->SetMaterial(0, MaterialAsset.Object);
+    }
+
+    PrimaryActorTick.bCanEverTick = true;
+    MoveSpeed = 40.0f;
+    MaxRange = 50.0f;
+}
+
+void ADonut::BeginPlay()
+{
+    Super::BeginPlay();
+    StartLocation = GetActorLocation();
+    MoveDirection = 1; 
+}
+
+void ADonut::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (!FMath::IsNearlyZero(MoveSpeed))
+    {
+        FVector CurrentLocation = GetActorLocation();
+
+        FVector NewLocation = CurrentLocation + FVector(0.0f, 0.0f, MoveDirection * MoveSpeed * DeltaTime);
+
+        if (NewLocation.Z >= StartLocation.Z + MaxRange) 
+        {
+            NewLocation.Z = StartLocation.Z + MaxRange; 
+            MoveDirection = -1; 
+        }
+        else if (NewLocation.Z <= StartLocation.Z - MaxRange)
+        {
+            NewLocation.Z = StartLocation.Z - MaxRange;
+            MoveDirection = 1;
+        }
+
+        SetActorLocation(NewLocation);
+    }
+}
+
+void ADonut::ResetActorPosition()
+{
+    SetActorLocation(FVector::ZeroVector);
+}
+
+float ADonut::GetRotationSpeed() const
+{
+    return MoveSpeed;
+}
