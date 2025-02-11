0a67d84fd30b051c02d3b82b1c94672fd4eec905
aiden_shin <shinichi9784@gmail.com>
2025-02-11 22:06:54 +0900

    코드 수정
    부분 누락된 코드 수정

---
+++ Source/SpartaProject/Public/Donut.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Donut.generated.h"
+
+UCLASS()
+class SPARTAPROJECT_API ADonut : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ADonut();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Donut|Components")
+	USceneComponent* SceneRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Donut|Components")
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Donut|Properties")
+	float MoveSpeed;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Donut|Actions")
+	void ResetActorPosition();
+
+	UFUNCTION(BlueprintPure, Category = "Donut|Properties")
+	float GetRotationSpeed() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Donut|Event")
+	void OnItemPickedUp();
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float ActorMoveSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MaxRange;
+
+	FVector StartLocation;
+	int32 MoveDirection;
+
+	FTimerHandle ChangeMeshTimerHandle;
+	FTimerHandle DestroyTimerHandle;
+
+	UFUNCTION()
+	void ChangeMesh();
+
+	UFUNCTION()
+	void DestroyActor();
+};
